b3e608b7863c7e899325a91df6eef16d41457b67
Facundo Olesen <faolesen@alumnos.exa.unicen.edu.ar>
2023-12-09 17:02:19 -0300

    Revert "Change port"

--- router.php
+++ router.php
@@ -5,7 +5,7 @@ require_once 'app/controllers/auth.controller.php';
 
 
 
-define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER[37221] . dirname($_SERVER['PHP_SELF']) . '/');
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 
 
 $action = 'home';
